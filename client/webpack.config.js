c1f34efb4b9feafbbbfa90c2f8d11d5fa5050519
NiseAkki <niseakki@gmail.com>
2025-02-07 11:09:21 +0800

    优化排行榜更新逻辑: - 移除实时排行榜更新 - 在每回合开始时更新排行榜 - 删除交易后的排行榜更新 - 优化玩家加入游戏时的排行榜处理 - 修复排行榜显示问题

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,11 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    clean: true
   },
   module: {
     rules: [
@@ -14,19 +13,23 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
-    })
+      template: './public/index.html',
+    }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'public')
